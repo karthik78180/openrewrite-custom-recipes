d0b7b99826cd5d62fe3dcdbc6d1074d7cef3c78a
KAVUR <karthik78180@gmail.com>
2025-05-20 19:06:38 -0400

    Add composite recipie

--- src/test/java/com/rewrite/CompositeMigrationTest.java
+++ src/test/java/com/rewrite/CompositeMigrationTest.java
@@ -81,4 +81,45 @@ class CompositeMigrationTest implements RewriteTest {
                 )
         );
     }
+
+    @Test
+    void testCompositeIncludesAddStructured2() {
+        rewriteRun(
+                java(
+                        """
+                        package io.vertx.core;
+        
+                        public interface Promise<T> {
+                        }
+                        """
+                ),
+                // Mock Structured class
+                java(
+                        """
+                        public class Structured {
+                        }
+                        """
+                ),
+                java(
+                        """
+                        import io.vertx.core.Promise;
+        
+                        public class MyVerticle {
+                            public void start(Promise<Void> init) {
+                                System.out.println("Init called");
+                            }
+                        }
+                        """,
+                        """
+                        import io.vertx.core.Promise;
+        
+                        public class MyVerticle {
+                            public void start(Promise<Void> init, Structured structured) {
+                                System.out.println("Init called");
+                            }
+                        }
+                        """
+                )
+        );
+    }
 }
